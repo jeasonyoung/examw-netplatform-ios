3cf5a682139abe6f93091fc0d30984595f40c6fe
jeasonyoung <jeason1914@qq.com>
2015-11-25 09:33:38 +0800

    修改版权信息

--- NetSchool/Classes/登录注册/LoginViewController.m
+++ NetSchool/Classes/登录注册/LoginViewController.m
@@ -53,7 +53,7 @@
 {
     [super viewDidLoad];
   
-    NSString *text = @"2006-2015 长沙畅亨信息技术有限公司";
+    NSString *text = @" © 2015 长沙品学网络科技有限公司 ";
     CGSize size = [NSObject getSizeWithText:text font:FontBold(17) maxSize:CGSizeMake(DeviceW, FontBold(15).lineHeight)];
     UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake((DeviceW - size.width) / 2, DeviceH - kDefaultInset.bottom * 2 - FontBold(17).lineHeight , size.width, size.height)];
     title.font = FontBold(17);
